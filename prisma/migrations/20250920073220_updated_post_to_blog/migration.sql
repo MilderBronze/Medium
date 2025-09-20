3013e999ade63152305575398df7bbc3f20fa1cc
MilderBronze <sumitinbki@gmail.com>
2025-09-20 15:27:07 +0530

    zod validation added

---
+++ prisma/migrations/20250920073220_updated_post_to_blog/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "public"."Post";
+
+-- CreateTable
+CREATE TABLE "public"."Blog" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Blog" ADD CONSTRAINT "Blog_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
+// import { PrismaClient } from './generated/prisma/client'
+import { PrismaClient } from './generated/prisma/edge'
+// import { PrismaClient } from '@prisma/client'
+import z from 'zod'
+
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { User, Post } from './generated/prisma'
+import { sha256 } from 'hono/utils/crypto'
+// import { User, Blog } from './generated/prisma' // iska zarurat nai padega
 
 
 type Bindings = {
@@ -11,11 +16,20 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>()
 
+export const getPrisma = (database_url: string) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: database_url,
+  }).$extends(withAccelerate())
+  return prisma
+}
+
 // routes to make:
+
+app.get('/', (c) => c.text('Hello Cloudflare Workers!'))
+
 
 // 1. get all posts
 app.get('/api/v1/blog/bulk', (c) => {
-  const prisma = new PrismaClient().$extends(withAccelerate())
 
 })
 
@@ -38,9 +52,60 @@ app.get('/api/v1/blog', (c) => {
 
 // auth routes
 // 5. signup route
-app.post('/api/v1/user/signup', (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  // for future:
   // receive user credentials from req.body
   // validate if user already present agains the db -> yes: throw error -> no: {check if user email legit (send an otp to his email) -> user enters otp within the 5 mins window and verifies himself -> yes: (create his account, generate jwt, store it in his browser, redirect to dashboard) -> no: redirect back to the signin route
+
+
+  // for now:
+  // receive user credentials from req.body
+  // if user exists... throw
+  // else create account
+  const prisma = getPrisma(c.env.DATABASE_URL)
+
+  const { email, password } = await c.req.json();
+  try {
+    const userFound = await prisma.user.findFirst({
+      where: { email }
+    });
+    if (userFound) {
+      return c.json({
+        Message: "User already exists!",
+      }, 411);
+    }
+
+    const hashedPassword = await sha256(password) as string
+
+    // add zod validation here.
+    const User = z.object({
+      name: z.string().optional(),
+      email: z.string().email(),
+      password: z.string()
+    });
+
+    // some untrusted data...
+    const input = {
+      email: email,
+      password: hashedPassword
+    };
+
+    // the parsed result is validated and type safe!
+    const data = User.parse(input);
+    const userCreatedSuccessfully = await prisma.user.create({
+      data
+    })
+    return c.json({
+      success: true,
+      user: userCreatedSuccessfully
+    })
+  } catch (error) {
+    return c.json({
+      error: error,
+      success: false,
+      message: "internal server error"
+    }, 500)
+  }
 })
 
 // 6. signin route
