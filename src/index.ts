7748126768a59163d04e75d71c4702350caf12c5
MilderBronze <sumitinbki@gmail.com>
2025-09-20 19:57:29 +0530

    added routing userRouter and blogRouter

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono'
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from './generated/prisma/edge'
+
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { User, Post } from './generated/prisma'
+import userRouter from './routes/user'
+import { blogRouter } from './routes/blog'
 
 
 type Bindings = {
@@ -10,45 +12,14 @@ type Bindings = {
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
-// routes to make:
+export const getPrisma = (database_url: string) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: database_url,
+  }).$extends(withAccelerate())
+  return prisma
+}
 
-// 1. get all posts
-app.get('/api/v1/blog/bulk', (c) => {
-  const prisma = new PrismaClient().$extends(withAccelerate())
-
-})
-
-
-// 2. upload a post
-app.post('/api/v1/blog', (c) => {
-
-})
-
-
-// 3. get a specific post
-app.get('/api/v1/blog/:id', (c) => {
-
-})
-
-// 4. update a blog
-app.get('/api/v1/blog', (c) => {
-
-})
-
-// auth routes
-// 5. signup route
-app.post('/api/v1/user/signup', (c) => {
-  // receive user credentials from req.body
-  // validate if user already present agains the db -> yes: throw error -> no: {check if user email legit (send an otp to his email) -> user enters otp within the 5 mins window and verifies himself -> yes: (create his account, generate jwt, store it in his browser, redirect to dashboard) -> no: redirect back to the signin route
-})
-
-// 6. signin route
-app.post('/api/v1/user/signin', (c) => {
-
-})
-
-
-
-
-export default app
+export default app;
