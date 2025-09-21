84a97becf3506f767e2ba12bf23e20fd6695cd76
MilderBronze <sumitinbki@gmail.com>
2025-09-21 10:54:51 +0530

    added pagination to the blogs

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,16 +1,26 @@
 import { Hono } from "hono";
-import { decode, jwt, verify } from "hono/jwt";
+import { verify } from "hono/jwt";
+import { getPrisma } from "..";
+import z, { string } from 'zod'
 
 type Bindings = {
     DATABASE_URL: string
     SECRET_KEY: string
 }
+
 interface jwtPayload { id: string; email: string }
 
 type Variables = {
     jwtPayload?: jwtPayload;
 };
 export const blogRouter = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+
+// the goal here is.. to pass in only the user modified values..
+const removeUndefined = <T extends object>(obj: T): Partial<T> => {
+    return Object.fromEntries(
+        Object.entries(obj).filter(([_, v]) => v !== undefined)
+    ) as Partial<T>
+}
 
 blogRouter.use('/*', async (c, next) => {
     try {
@@ -41,14 +51,87 @@ blogRouter.use('/*', async (c, next) => {
 })
 
 // 1. get all posts
-blogRouter.get('/bulk', (c) => {
-
+blogRouter.get('/bulk', async (c) => {
+    try {
+        const prisma = getPrisma(c.env.DATABASE_URL)
+        const page = Number(c.req.query("page")) || 1;
+        const pageSize = 50;
+        // --- Total blogs count for pagination info ---
+        const totalBlogs = await prisma.blog.count();
+        const totalPages = Math.ceil(totalBlogs / pageSize);
+        const blogs = await prisma.blog.findMany({
+            skip: (page - 1) * pageSize,
+            take: pageSize,
+            include: { author: true },
+            orderBy: { createdAt: 'desc' }
+        });
+        return c.json({
+            success: true,
+            message: "all blogs retrieved",
+            blogs,
+            pagination: {
+                page,
+                pageSize,
+                totalBlogs,
+                totalPages,
+                hasNextPage: page < totalPages,
+                hasPrevPage: page > 1
+            }
+        }, 200)
+    } catch (error) {
+        return c.json({
+            success: false,
+            message: "blogs failed to be retrieved"
+        }, 500)
+    }
 })
 
 
 // 2. upload a post
-blogRouter.post('/', (c) => {
-
+blogRouter.post('/', async (c) => {
+    try {
+        const prisma = getPrisma(c.env.DATABASE_URL)
+        const { title, content } = await c.req.json();
+        const user = c.get('jwtPayload');
+        if (!user) {
+            return c.json({
+                success: false,
+                message: "jwtPayload didnt have user email/id"
+            }, 404)
+        }
+        const blogSchema = z.object({
+            title: z.string(),
+            content: z.string(),
+            authorId: z.number()
+        })
+        const validatedData = blogSchema.parse({
+            title: title,
+            content: content,
+            authorId: parseInt(user.id)
+        })
+        const blog = await prisma.blog.create({
+            data: validatedData
+        })
+        return c.json({
+            message: "blog creation success",
+            blog: blog,
+            blogId: blog.id,
+            success: true,
+        }, 200)
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return c.json({
+                message: "input validation failed",
+                success: false,
+                error: error.errors
+            })
+        }
+        return c.json({
+            message: "blog not created",
+            success: false,
+            error: error
+        })
+    }
 })
 
 
@@ -57,8 +140,61 @@ blogRouter.get('/:id', (c) => {
 
 })
 
-// 4. update a blog
-blogRouter.get('', (c) => {
+blogRouter.put('/:id', async (c) => {
+    try {
+        const blogUpdateSchema = z.object({
+            title: z.string().optional(),
+            content: z.string().optional()
+        })
 
+        const prisma = getPrisma(c.env.DATABASE_URL)
+        const { title, content } = await c.req.json();
+        // values related to blog to be passed in req.body
+        const blogId: number = Number(c.req.param("id"));
+        if (isNaN(blogId)) {
+            return c.json({ message: "Invalid blog ID", success: false });
+        }
+
+        const blogFound = await prisma.blog.findUnique({
+            where: { id: blogId }
+        })
+        if (!blogFound) {
+            return c.json({
+                message: "blog not found",
+                success: false
+            })
+        }
+
+        const verifiedData = blogUpdateSchema.parse({
+            title, content
+        })
+        const data = removeUndefined(verifiedData)
+
+        if (Object.keys(data).length === 0) {
+            return c.json({ message: "no fields to update", success: false })
+        }
+
+        const blog = await prisma.blog.update({
+            where: { id: blogId },
+            data: data
+        })
+        return c.json({
+            message: "blog updated",
+            success: true,
+            blog: blog
+        })
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return c.json({
+                message: "update request failed due to invalid data input",
+                success: false,
+                error: error.errors
+            })
+        }
+        return c.json({
+            message: "failed to update blog",
+            success: false,
+            error: error
+        })
+    }
 })
-
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -18,7 +18,6 @@ userRouter.post('/signup', async (c) => {
     // receive user credentials from req.body
     // validate if user already present agains the db -> yes: throw error -> no: {check if user email legit (send an otp to his email) -> user enters otp within the 5 mins window and verifies himself -> yes: (create his account, generate jwt, store it in his browser, redirect to dashboard) -> no: redirect back to the signin route
 
-
     // for now:
     // receive user credentials from req.body
     // if user exists... throw
